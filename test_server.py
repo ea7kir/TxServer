a2dfb91090a2834109b3326cc09c73b1b53a30dd
ea7kir <mikenaylorspain@icloud.com>
2023-01-19 11:54:53 +0100

    almost

--- test_server.py
+++ test_server.py
@@ -16,24 +16,32 @@ class RoofData:
 
 roof_data = RoofData()
 
-async def server(websocket): #, path):
-    print('A client just connected', flush=True)
-    roof_data.connected = True
-    try:
-        while True:
-            command = await websocket.recv()
-            print(command, flush=True)
+###############################################################
+def OLD_process_run_server(connection):
+    async def server(websocket): #, path):
+        print('A client just connected', flush=True)
+        roof_data.connected = True
+        try:
+            while True:
+                command = await websocket.recv()
+                print(command, flush=True)
 
-            await websocket.send(f'{roof_data.counter}') # TODO: needs JSON
-            roof_data.counter += 1
-            sleep(1.0)
-    # Handle disconnecting clients 
-    except websockets.exceptions.ConnectionClosed as e:
-        print("A client just disconnected", flush=True)
-        roof_data.connected = False
-    finally:
-        roof_data.connected = False
+                await websocket.send(f'{roof_data.counter}') # TODO: needs JSON
+                roof_data.counter += 1
+                sleep(1.0)
+        # Handle disconnecting clients 
+        except websockets.exceptions.ConnectionClosed as e:
+            print("A client just disconnected", flush=True)
+            roof_data.connected = False
+        finally:
+            roof_data.connected = False
 
-start_server = websockets.serve(server, "0.0.0.0", PORT)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+    start_server = websockets.serve(server, "0.0.0.0", PORT)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+###############################################################
+
+def NEW_process_run_server(connection):
+    pass
+
+OLD_process_run_server(999)
