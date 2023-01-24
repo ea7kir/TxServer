a6ef20c6245f7df02c44a85a7cde37bd998a0b61
ea7kir <mikenaylorspain@icloud.com>
2023-01-24 12:19:08 +0100

    adding pipgpio

--- switch_relays.py
+++ switch_relays.py
@@ -1,50 +1,36 @@
-"""
-Use a GPIO library built on libgpiod
-
-eg: apt install pigpio python-pigpio python3-pigpio.
-    ( or compile latest version: https://github.com/joan2937/pigpio )
-
-    then:
-
-    import pigpio
-    import time
-    ledPin = 6
-    pi = pigpio.pi()
-    pi.set_mode(ledPin, pigpio.OUTPUT)
-    while (True):
-        pi.write(ledPin, True)
-        time.sleep(1)
-        pi.write(ledPin, False)
-        time.sleep(1)
-"""
 import pigpio
 
-pi = pigpio.pi()
+RELAY_28v_PIN   = 1 # TODO: what pin to use
+RELAY_12v_PIN   = 2  
+RELAY_5v_PIN    = 3
 
-RELAY_28v_ADDRESS   = 1
-RELAY_12v_ADDRESS   = 2  
-RELAY_5v_ADDRESS    = 3
-
-ON = 0
+ON = 0  # TODO: int or bool?
 OFF = 1
 
-def _switch_relay(address, state):
+# pi = pigpio.pi() # TODO: need to close ?
+
+# pi.set_mode(RELAY_28v_PIN, pigpio.OUTPUT)
+# pi.set_mode(RELAY_12v_PIN, pigpio.OUTPUT)
+# pi.set_mode(RELAY_5v_PIN, pigpio.OUTPUT)
+
+def _switch_relay(pin, state):
     pass
+    # pi.write(pin, state)
 
 def switch_28v_On():
-    _switch_relay(RELAY_28v_ADDRESS, ON)
+    _switch_relay(RELAY_28v_PIN, ON)
 
 def switch_28v_Off():
-    _switch_relay(RELAY_28v_ADDRESS, OFF)
+    _switch_relay(RELAY_28v_PIN, OFF)
 
 def switch_12v_On():
-    _switch_relay(RELAY_12v_ADDRESS, ON)
+    _switch_relay(RELAY_12v_PIN, ON)
 
 def switch_12v_Off():
-    _switch_relay(RELAY_12v_ADDRESS, OFF)
+    _switch_relay(RELAY_12v_PIN, OFF)
 
 def switch_5v_On():
-    _switch_relay(RELAY_5v_ADDRESS, ON)
+    _switch_relay(RELAY_5v_PIN, ON)
 
 def switch_5v_Off():
-    _switch_relay(RELAY_5v_ADDRESS, OFF)
+    _switch_relay(RELAY_5v_PIN, OFF)
